36e3d0c0359c789d269662dd3d741bc3c9fc0b3c
lincindiawang <lindawang0207@gmail.com>
2019-10-29 16:39:58 +0800

    1st commit

---
+++ app/src/main/java/com/linda/example/MainActivity.java
@@ -0,0 +1,75 @@
+package com.linda.example;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView name;
+    private TextView password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        name = findViewById(R.id.ed_name);
+        password = findViewById(R.id.ed_password);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void Button(View view) {
+        if (name.length() > 6) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setMessage("Your name have to be less than 6 letters")
+                    .setPositiveButton("OK", null)
+                    .show();
+        } else if (password.length() < 5) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setMessage("Your password have to be more than 5 words")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
